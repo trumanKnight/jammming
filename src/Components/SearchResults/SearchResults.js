4e640436a2c517f44d4ca8ad9406c70070e93c76
Truman Knight <cvilletruman@gmail.com>
2022-05-28 13:06:54 -0400

    Make static components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Tracklist } from '../Tracklist/Tracklist';
+
+import './SearchResults.css';
+
+export class SearchResults extends React.Component {
+    render() {
+        return(
+            <div className="SearchResults">
+                <h2>Results</h2>
+                <Tracklist />
+            </div>
+        )
+    }
+}
---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Tracklist.css';
+
+export class Tracklist extends React.Component {
+    render() {
+        return(
+            <div className="Tracklist">
+                
+            </div>
+        )
+    }
+}
